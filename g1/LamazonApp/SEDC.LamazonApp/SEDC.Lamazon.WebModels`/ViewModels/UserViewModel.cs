d2cd1b39845de8873e032cdc7fb6d40ff4599fdd
Dejan <djovanov92@gmail.com>
2019-09-14 12:21:54 +0200

    class 03 stage 2

---
+++ g1/LamazonApp/SEDC.LamazonApp/SEDC.Lamazon.WebModels`/ViewModels/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Lamazon.WebModels_.ViewModels
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+    }
+}
