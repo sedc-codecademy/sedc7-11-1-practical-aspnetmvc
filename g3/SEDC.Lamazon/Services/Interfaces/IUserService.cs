e59c67b61aad0d126f01296858a2530d55fb6b5e
Design <design@sedc.mk>
2019-09-28 12:48:49 +0200

    users controller created

--- g3/SEDC.Lamazon/Services/Interfaces/IUserService.cs
+++ g3/SEDC.Lamazon/Services/Interfaces/IUserService.cs
@@ -5,5 +5,8 @@ namespace Services.Interfaces
     public interface IUserService
     {
         void Register(RegisterViewModel model);
+        void Login(LoginViewModel model);
+        void Logout();
+        UserViewModel GetCurrentUser(string username);
     }
 }
